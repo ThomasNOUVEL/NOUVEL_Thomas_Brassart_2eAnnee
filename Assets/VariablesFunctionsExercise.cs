dce947527985514e40a505713843a27c2a74cbdc
ThomasNOUVEL <thomas.nouvel@brassart.fr>
2025-11-27 13:51:09 +0100

    create scrypt

---
+++ Assets/VariablesFunctionsExercise.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class VariablesFunctionsExercise : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 1. a.
+        int tata = 5;
+        int toto = 7;
+
+        print(tata);
+        print(toto);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
