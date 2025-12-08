58a2105fedb61885aee52f97f1177962d1a898a2
ThomasNOUVEL <thomas.nouvel@brassart.fr>
2025-12-08 14:56:14 +0100

    rectification fonctions

--- Assets/Scenes 1/TestLanguage.cs
+++ Assets/Scenes 1/TestLanguage.cs
@@ -27,41 +27,7 @@ public class TestLanguage : MonoBehaviour
 
 
 
-        int Increment(int value) // int informe que le return va etre un entier
-        {
-            return value + 1;
-        }
-
-
-
-        int Heal(int health, int potion = 5) // int health, int potion    informe que la fonction demande deux entiers pour faire ses calcules. Ici, "potion" = 10 si il n est pas spécifié à l'appel.
-        {
-            if health += potion > MAX_HEALTH_POINTS
-                health = MAX_HEALTH_POINTS;
-            else
-                health += potion;
-            return health;
-        }
-
-
         
-        (int, bool) Damage(int health, int Dgt, bool Life = true) // (int, bool)    informe que le return est un entier et un booléun    et     int health, int Dgt, bool Life     informe que la fonction demande deux entiers et un booléun pour faire ses calcules
-        {
-            if health -= dDgt < 0
-                health = 0
-                Life = false;
-            else
-                health -= Dgt;
-            return (health, Life)
-        }
-
-
-
-        void CreerVar(out int Variable) // void    informe que le return sera vide    et     out informe que la variable sera créer dans cette fonction mais sera accessible en dehors
-        {
-            int Variable = 200
-                return
-        }
 
 
 
@@ -71,5 +37,41 @@ public class TestLanguage : MonoBehaviour
 
 
     }
+
+    int Increment(int value) // int informe que le return va etre un entier
+    {
+        return value + 1;
+    }
+
+
+
+    int Heal(int health, int potion = 5) // int health, int potion    informe que la fonction demande deux entiers pour faire ses calcules. Ici, "potion" = 10 si il n est pas spécifié à l'appel.
+    {
+        if health += potion > MAX_HEALTH_POINTS
+                health = MAX_HEALTH_POINTS;
+        else
+            health += potion;
+        return health;
+    }
+
+
+
+    (int, bool) Damage(int health, int Dgt, bool Life = true) // (int, bool)    informe que le return est un entier et un booléun    et     int health, int Dgt, bool Life     informe que la fonction demande deux entiers et un booléun pour faire ses calcules
+    {
+        if health -= dDgt < 0
+                health = 0
+                Life = false;
+            else
+            health -= Dgt;
+        return (health, Life)
+        }
+
+
+
+    void CreerVar(out int Variable) // void    informe que le return sera vide    et     out informe que la variable sera créer dans cette fonction mais sera accessible en dehors
+    {
+        int Variable = 200
+                return
+        }
 
 }
